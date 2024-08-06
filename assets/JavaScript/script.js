54db0803f712df196a5d854401edee5150ba421f
Matt <70668167+MattJhet@users.noreply.github.com>
2024-08-06 19:33:20 -0400

    added all functions
    something isn't working idk if its because i just got my api key tho, still need to do some styling and find out those errors

--- assets/JavaScript/script.js
+++ assets/JavaScript/script.js
@@ -32,7 +32,12 @@ function getWeather() {
             alert('Error fetching hourly weather data. Please try again.');
         })
 }
+function showIamge(){
+    const weatherIcon = document.getElementById('weather-icon');
+    weatherIcon.style.display = 'block';
+}
 
+// take weather data extract relevant information and update HTML elements within the current weather details and display issues if theres a error with the API request
 function displayWeather(data) {
 
     const tempDivInfo = document.getElementById('temp-div');
@@ -44,4 +49,53 @@ function displayWeather(data) {
     weatherInfoDiv.innerHTML ='';
     hourlyForecastDiv.innerHTML ='';
     tempDivInfo.innerHTML ='';
+
+    
+    if(data.cod ==='404') {
+        weatherInfoDiv.innerHTML = `<p>${data.message}</p>`;
+    } else {
+        const cityName = data.name;
+        const temperature = Math.round(data.main.temp - 273.15);
+        const description = data.weather[0].description;
+        const iconCode = data.weather[0].icon;
+        const iconUrl = `https://openweathermap.org/img/wn/${iconCode}@4x.png`;
+
+        const temperatureHTML = `
+        <p>${temperature}°C</p>
+        `;
+        const weatherHTML = `
+            <p>${cityName}</p>
+            <p>${descriptionc}</p>
+        `
+        tempDivInfo.innerHTML = temperatureHTML;
+        weatherInfoDiv.innerHTML = weatherHTML;
+        weatherIcon.src = iconUrl;
+        weatherIcon.alt = description;
+
+        showImage();
+    }
+
+    
+}
+
+
+function displayHourlyForecast(hourlyData) {
+    const hourlyForecastDiv = document.getElementById('hourly-forecast');
+    const next24Hours = hourlyData.slice(0, 8);
+
+    next24Hours.forEach(item => {
+        const dateTime = new Date(item.dt * 1000);
+        const hour = dateTime.getHours();
+        const temperature = Math.round(item.main.temp - 273.15);
+        const iconCode = data.weather[0].icon;
+        const iconUrl = `https://openweathermap.org/img/wn/${iconCode}@4x.png`;
+
+        const hourlyItemHTML = `
+        <div class="hourly-item">
+            <span>${hour}:00</span>
+            <img src="${iconUrl}" alt="Hourly Weather Icon">
+            <span>${temperature}°C</span>
+        `;
+        hourlyForecastDiv.innerHTML += hourlyItemHTML;
+    });
 }
