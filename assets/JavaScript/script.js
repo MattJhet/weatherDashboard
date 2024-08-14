b4ee98ba545722b89cd35f0ed8ae6ed408a928ea
Matt <70668167+MattJhet@users.noreply.github.com>
2024-08-14 13:36:00 -0400

    reworking some html and css

--- assets/JavaScript/script.js
+++ assets/JavaScript/script.js
@@ -11,7 +11,7 @@ function getWeather() {
     }
 
     const currentWeatherUrl =`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apikey}`;
-    const forecastUrl =`https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${apiKey}`;/
+    const forecastUrl =`https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${apiKey}`;
 
     fetch(currentWeatherUrl) 
         .then(response => response.json())
